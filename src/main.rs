d1f1045826832a69958edaf3961f54cd6f9a0e20
Vinermy <egorkosachev@vk.com>
2023-12-23 21:15:38 +0700

    Basic editor online!

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
-use iced::{Element, Sandbox, Settings};
-use iced::widget::text;
+use iced::{Theme, Element, Sandbox, Settings};
+use iced::widget::{container, text_editor};
 
 fn main() -> iced::Result {
     Editor::run(Settings::default())
 }
 
-struct Editor;
-#[derive(Debug)]
-enum Message {}
+struct Editor {
+    content: text_editor::Content,
+}
+#[derive(Debug, Clone)]
+enum Message {
+    Edit(text_editor::Action)
+}
 
 impl Sandbox for Editor {
     type Message = Message;
 
     fn new() -> Self {
-        Self
+        Self {
+            content: text_editor::Content::with(include_str!("main.rs")),
+        }
     }
 
     fn title(&self) -> String {
@@ -21,10 +27,19 @@ impl Sandbox for Editor {
     }
 
     fn update(&mut self, message: Message) {
-        match message { _ => {} }
+        match message {
+            Message::Edit(action) => {
+                self.content.edit(action);
+            }
+        }
     }
 
     fn view(&self) -> Element<'_, Message> {
-        text("Hello, iced!").into()
+        let input = text_editor(&self.content).on_edit(Message::Edit);
+        container(input).padding(10).into()
+    }
+
+    fn theme(&self) -> Theme {
+        Theme::Dark
     }
 }
